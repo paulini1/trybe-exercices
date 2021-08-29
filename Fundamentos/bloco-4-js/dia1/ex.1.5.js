cf46e31c02319f7ecd517f13f4ba4c0f84ed19f4
Paulini <guiguigui45@hotmail.com>
2021-08-28 21:15:41 -0300

    Exercicios 5.

---
+++ Fundamentos/bloco-4-js/dia1/ex.1.5.js
@@ -0,0 +1,5 @@
+const a=8;
+const b=2;
+let resultado = undefined;
+resultado = 8%3;
+console.log(resultado);
---
+++ Fundamentos/bloco-4-js/dia1/ex1.3.js
@@ -0,0 +1,5 @@
+const a=4;
+const b=6;
+let resultado = undefined;
+resultado = a * b;
+console.log(resultado);
