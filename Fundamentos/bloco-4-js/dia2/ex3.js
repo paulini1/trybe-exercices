bfe0d7254f9fd9f89d32f9779e15a4b79cc7617f
Paulini <guiguigui45@hotmail.com>
2021-09-08 21:58:54 -0300

    ex3.

---
+++ Fundamentos/bloco-4-js/dia2/ex3.js
@@ -0,0 +1,9 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let resultado = 0;
+let contador =0;
+for(let index = 0 ; index < numbers.length ; index += 1) {
+    
+  resultado =numbers[index] + resultado ;
+  contador = numbers.length;
+}console.log("Soma de todos os números do array: " + resultado);
+console.log("Média aritmética :" + resultado/contador);
