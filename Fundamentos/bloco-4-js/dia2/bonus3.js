87dea13297c1b39804ec80b75157d30e2cd2ea76
Paulini <guiguigui45@hotmail.com>
2021-09-09 18:14:19 -0300

    exercicio bonus 3.

--- Fundamentos/bloco-4-js/dia2/bonus3.js
+++ Fundamentos/bloco-4-js/dia2/bonus3.js
@@ -1,19 +1,12 @@
 //multiplicar o primeiro numero do array com o segundo numero e assim sucessivamente.
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let array1 = [];
-let array2 = [];
-let resultado =0
-for(let index = 0 ; index < numbers.length ; index += 1){
-    
-        
-        resultado = numbers[index] * numbers[index +1]
-        
-    
-    
-    array1.push(resultado)
-        
 
-    
+for(let index = 0; index < numbers.length ; index += 1){
+    if(numbers[index] === numbers[numbers.length -1]){
+        array1.push(numbers[index] * 2);
+    }else{
+        array1.push(numbers[index] * numbers[index +1]);
+    }
 }
-
-console.log(array1)
+console.log(array1);
